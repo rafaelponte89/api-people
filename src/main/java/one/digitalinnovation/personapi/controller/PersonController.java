94b53ca72509687c198cd6e8e74ec2b963263f2c
Rafael Ponte Casemiro <rafael_ponte1b@hotmail.com>
2021-07-01 18:09:13 -0400

    Developed delete by Id operation

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -37,5 +37,11 @@ public class PersonController {
         return personService.findById(id);
     }
 
+   @DeleteMapping("/{id}")
+   @ResponseStatus(HttpStatus.NO_CONTENT)
+   public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+   }
+
 
 }
